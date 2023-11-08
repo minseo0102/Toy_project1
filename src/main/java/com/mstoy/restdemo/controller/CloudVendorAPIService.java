f021ddcf5ffffa312a672e99e38047815da4d504
minseo0102 <sk49058275@gmail.com>
2023-11-08 19:55:51 +0900

    post logic successfully operate & get method update

--- src/main/java/com/mstoy/restdemo/controller/CloudVendorAPIService.java
+++ src/main/java/com/mstoy/restdemo/controller/CloudVendorAPIService.java
@@ -1,17 +1,22 @@
 package com.mstoy.restdemo.controller;
 
 import com.mstoy.restdemo.model.CloudVendor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cloudvendor")
 public class CloudVendorAPIService {
+    CloudVendor cloudVendor;
     @GetMapping("{vendorId}")
     public CloudVendor getCloudVendorDetails(String vendorId)
     {
-        return new CloudVendor("C1", "Vendor 1",
-                "Address One", "xxxxx");
+        return cloudVendor;
+    }
+
+    @PostMapping
+    public String postCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+    {
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Created Successfully";
     }
 }
